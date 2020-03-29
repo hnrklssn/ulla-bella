a42bea3dbce848b4ad2d1e677563942ff6138389
Henrik Olsson <hnrklssn@gmail.com>
2020-03-29 09:50:50 +0000

    add help queue

--- src/UllaBella.ts
+++ src/UllaBella.ts
@@ -14,6 +14,8 @@ function NotBot(message: Message, client: Client) {
 }
 @Discord({ prefix: "!" })
 abstract class AppDiscord {
+    private helpQ = [];
+
     @Command("hello")
     @Guard(NotBot)
     private hello(
@@ -21,7 +23,23 @@ abstract class AppDiscord {
         client: Client
     ) {
         console.log("command received");
+        console.log("msg: ", message);
         message.reply("Hello!");
+    }
+
+    @Command("help")
+    @Guard(NotBot)
+    private addHelp(
+        message: CommandMessage,
+        client: Client
+    ) {
+        console.log("!help received");
+        if(this.helpQ.includes(message.author)) {
+            message.reply(["You are already in line for help.", "You can remove yourself from the queue with !removeHelp, or view the queue with !showHelp"]);
+            return;
+        }
+        const len = this.helpQ.push(message.author);
+        message.reply(["You are now in line for help.", this.queuePositionText(len-1)]);
     }
 
     @CommandNotFound()
@@ -31,10 +49,21 @@ abstract class AppDiscord {
         client: Client
     ) {
         message.reply("Command not found");
+        console.log("msg: ", message);
     }
 
     @On("ready")
     private ready() {
         console.log("Ready");
+    }
+
+    private queuePositionText(position: Number) {
+        if(position == 0) {
+            return "You are first in line.";
+        }
+        if(position == 1) {
+            return "There is one person before you.";
+        }
+        return `There are ${position} people before you.`;
     }
 }
